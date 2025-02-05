a71e9c6dded74d22ba46709ef2da54ecd7ca901e
r-sakuraba <ruki.bloom@gmail.com>
2025-02-06 06:13:09 +0900

    sample

--- app.js
+++ app.js
@@ -1,74 +1,27 @@
-const WebSocket = require('ws');
-const { Buffer } = require('buffer');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-// WebSocketサーバーをポート8080で作成
-const wss = new WebSocket.Server({ port: 8080 });
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 
-// メッセージ履歴を保存する配列（最大10件）
-const messageHistory = [];
+const PORT = 3000;
 
-const MAX_HISTORY = 1000;
-
-wss.on('connection', (ws) => {
-  console.log('クライアントが接続しました。');
-
-  // 過去の10件のメッセージを接続したクライアントに送信
-  messageHistory.forEach((message) => {
-    ws.send(message);
-  });
-
-  // 接続している全てのクライアントにメッセージを送信する関数
-  const broadcast = (data) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  };
-
-  // クライアントからメッセージを受け取ったときの処理
-  ws.on('message', (message) => {
-    const buffer = Buffer.from(message, 'utf8');
-    const text = buffer.toString('utf8');
-    console.log('受信したメッセージ: ', text);
-
-    // メッセージをテキスト形式で履歴に追加
-    const textMessage = message.toString(); // バッファを文字列に変換
-    messageHistory.push(textMessage);
-
-    // メッセージ履歴が10件を超えたら古いものを削除
-    if (messageHistory.length > MAX_HISTORY) {
-      messageHistory.shift(); // 最古のメッセージを削除
-    }
-
-    // 全てのクライアントにメッセージを送信
-    broadcast(textMessage);
-  });
-
-  // 接続終了時
-  ws.on('close', () => {
-    console.log('クライアントが切断しました。');
-  });
-
-  // 初回接続時にクライアントへメッセージ送信
-  ws.send('チャットサーバーに接続されました。');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-console.log('WebSocketチャットサーバーがポート8080で起動中...');
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
-// renderのsleep対策
-// const URL_SERVER = `https://ws-example.onrender.com/`;
-// const URL_CLIENT = `https://ws-example-client.onrender.com/`;
-// const interval = 840000; // 14 minutes
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('sendMessage', (message) => {
+    console.log('Message has been sent: ', message);
 
-// async function reloadWebsite(url) {
-//   try {
-//     const response = await fetch(url);
-//     console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-//   } catch (error) {
-//     console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-//   }
-// }
-
-// setInterval(() => reloadWebsite(URL_SERVER), interval);
-// setInterval(() => reloadWebsite(URL_CLIENT), interval);
+    // 'receiveMessage' というイベントを発火、受信したメッセージを全てのクライアントに対して送信する
+    io.emit('receiveMessage', message);
+  });
+});
